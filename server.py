9ddd92a2b24c58e48e728847b5d07f9391f0f658
Aymanbalaa <84538300+Aymanbalaa@users.noreply.github.com>
2024-11-08 15:02:24 -0500

    register and register peersss

--- server.py
+++ server.py
@@ -2,12 +2,18 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 import threading
+from pymongo import MongoClient
+
+MONGO_URI = "mongodb+srv://admin:w5HI4gOqP1a3QHC4@cluster0.rpi2b.mongodb.net/p2p_shopping?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(MONGO_URI)
+db = client["p2p_shopping"]  
+peers_collection = db["registered_peers"]  
 
 class ThreadedHTTPServer(HTTPServer):
     def process_request(self, request, client_address):
         thread = threading.Thread(target=self.__new_connection, args=(request, client_address))
         thread.start()
-        thread.join() 
+        thread.join()
 
     def __new_connection(self, request, client_address):
         self.finish_request(request, client_address)
@@ -21,17 +27,52 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(json.dumps(response).encode('utf-8'))
 
-    def do_GET(self):
+    def do_POST(self):
         parsed_path = urlparse(self.path)
-        if parsed_path.path == '/hello':
-            self._send_response({'message': 'Hello, world!'})
-        elif parsed_path.path == '/items':
-            query_params = parse_qs(parsed_path.query)
-            item = query_params.get('item', [''])[0]
-            response = {'requested_item': item}
-            self._send_response(response)
+
+        if parsed_path.path == '/register':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            peer_info = json.loads(post_data)
+
+            name = peer_info.get("name")
+            ip_address = peer_info.get("ip_address")
+            udp_socket = peer_info.get("udp_socket")
+            tcp_socket = peer_info.get("tcp_socket")
+
+            # check if the user's name is already registeredd
+            if peers_collection.find_one({"name": name}):
+                response = {"error": "Name already in use."}
+                self._send_response(response, 400)
+            else:
+                peers_collection.insert_one({
+                    "name": name,
+                    "ip_address": ip_address,
+                    "udp_socket": udp_socket,
+                    "tcp_socket": tcp_socket
+                })
+                response = {"message": "REGISTERED", "name": name}
+                self._send_response(response, 201)
+
+        # deregister the peer IF he exists
+        elif parsed_path.path == '/deregister':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            peer_info = json.loads(post_data)
+
+            name = peer_info.get("name")
+
+            # Remove peer from MongoDB if it exists
+            if peers_collection.find_one({"name": name}):
+                peers_collection.delete_one({"name": name})
+                response = {"message": "DE-REGISTERED", "name": name}
+                self._send_response(response, 200)
+            else:
+                # if not found just ignore
+                response = {"error": "Name not registered."}
+                self._send_response(response, 400)
         else:
-            self._send_response({'error': 'Not found'}, 404)
+            self._send_response({"error": "Not found"}, 404)
 
 def run_server(server_class=ThreadedHTTPServer, handler_class=MyRequestHandler, port=3002):
     server_address = ('', port)
